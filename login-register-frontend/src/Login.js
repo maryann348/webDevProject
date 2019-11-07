ee40554e08816cb1febae5ae38064ff21ccaeb5d
camaymaryann123@gmail.com <camaymaryann@123@gmail.com>
2019-11-07 19:21:31 +0800

    Initial Commmit

--- login-register-frontend/src/Login.js
+++ login-register-frontend/src/Login.js
@@ -1,33 +1,69 @@
-import React from 'react'
-import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react'
+import React, {Component} from 'react'
+import { Button,  Form,} from 'semantic-ui-react'
+// import { Form } from 'semantic-ui-react'
+// import signUp from '../Component/signUp';
 
-const Login = () => (
-  <Segment placeholder>
-    <Grid columns={2} relaxed='very' stackable>
-      <Grid.Column>
-        <Form>
-          <Form.Input
-            icon='user'
-            iconPosition='left'
-            label='Username'
-            placeholder='Username'
-          />
-          <Form.Input
-            icon='lock'
-            iconPosition='left'
-            label='Password'
-            type='password'
-          />
 
-          <Button content='Login' primary />
-        </Form>
-      </Grid.Column>
 
-    </Grid>
+class Login extends Component {
+  state = {}
 
-    
-  </Segment>
-)
+  handleChange = (e, { value }) => this.setState({ value })
+
+  render() {
+    const { value } = this.state
+    return (
+      <Form>
+        <Form.Group   >
+          <Form.Input   placeholder='username' icon='user' iconPosition='left' className = 'input' />
+          <Form.Input   placeholder='password' icon='lock' iconPosition='left' type ='password' className = 'input' />
+          <Form.Button>Login</Form.Button>
+        </Form.Group>
+       
+      </Form>
+    )
+  }
+}
+
+
+
+// const options = [
+//   { key: 'm', text: 'Male', value: 'male' },
+//   { key: 'f', text: 'Female', value: 'female' },
+//   { key: 'o', text: 'Other', value: 'other' },
+// ]
+// class Login extends Component {
+//   state = {}
+
+//   handleChange = (e, { value }) => this.setState({ value })
+
+//   render() {
+//     const { value } = this.state
+//     return (
+//       <Form>
+//         <Form.Group widths='equal'>
+//           <Form.Input fluid label='First name' placeholder='First name' />
+//           <Form.Input fluid label='Middle name' placeholder='Middle name' />
+//           <Form.Input fluid label='Last name' placeholder='Last name' />
+//            <Form.Select
+//             fluid
+//             label='Gender'
+//             options={options}
+//             placeholder='Gender'
+//           />
+//           <Form.Input fluid label='Phone Number' placeholder='Phone Number' />
+//           <Form.Input fluid label='Email' placeholder='Email' />
+//            <Form.Input fluid label='Password' placeholder='Password' />
+//         </Form.Group>
+       
+       
+//         <Form.Button>SignUp</Form.Button>
+//       </Form>
+//     )
+//   }
+// }
+
+
 // render(){
 //   return (
 //     <div></div>
